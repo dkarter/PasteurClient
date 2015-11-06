64a9259d769a043f90636fbe08151b36e562dd93
Dorian Karter <dkarter@gmail.com>
2015-11-06 14:07:26 -0600

    Initial Commit

---
+++ Pasteur/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pasteur
+//
+//  Created by Dorian Karter on 2015-11-06.
+//  Copyright © 2015 Great Apes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
