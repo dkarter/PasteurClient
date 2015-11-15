0fbfd4925ec957be2fb0ee3f6994837bc6376420
Dorian Karter <dkarter@gmail.com>
2015-11-15 14:13:25 -0600

    toggle menu item visibility based on connection

--- Pasteur/AppDelegate.swift
+++ Pasteur/AppDelegate.swift
@@ -14,28 +14,47 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   private let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
   private let client:PasteurClient = PasteurClient(serverUrl: NSURL(string: "localhost:3000")!)
   private let notificationCenter = NSNotificationCenter.defaultCenter()
+  private var connectMenuItem:NSMenuItem = NSMenuItem()
   private var disconnectMenuItem:NSMenuItem = NSMenuItem()
+  private var publishMenuItem:NSMenuItem = NSMenuItem()
   
   func applicationDidFinishLaunching(aNotification: NSNotification) {
     if let button = statusItem.button {
       button.image = buildIcon()
       setMenubarButtonState(button, highlighted: false)
       statusItem.menu = buildMenu()
+      toggleMenuItemVisibility()
     }
     
     notificationCenter.addObserver(self, selector: "handleConnectionStateChange", name: "ConnectionStateChanged", object: client)
     
-    client.connect()
+    connect(self)
   }
  
   func handleConnectionStateChange() {
-    setMenubarButtonState(statusItem.button!, highlighted: client.isConnected())
-    let connectionStateText = client.isConnected() ? "connected to" : "disconnected from"
+    setMenubarButtonState(statusItem.button!, highlighted: client.isConnected)
+    toggleMenuItemVisibility()
+    let connectionStateText = client.isConnected ? "connected to" : "disconnected from"
     showNotification("Pasteur Server Connection", body: "Pasteur is now \(connectionStateText) the server")
   }
   
-  private func disconnect(sender: AnyObject) {
+  
+  func publishClipboard(sender: AnyObject) {
+    client.publishLocalClipboard()
+  }
+  
+  func disconnect(sender: AnyObject) {
     client.disconnect()
+  }
+  
+  func connect(sender: AnyObject) {
+    client.connect()
+  }
+  
+  private func toggleMenuItemVisibility() {
+    connectMenuItem.hidden = client.isConnected
+    disconnectMenuItem.hidden = !client.isConnected
+    publishMenuItem.hidden = !client.isConnected
   }
   
   private func buildIcon() -> NSImage {
@@ -46,15 +65,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   
   private func buildMenu() -> NSMenu {
     let menu = NSMenu()
-    menu.addItemWithTitle("Publish My Clipboard", action: Selector("publishClipboard:"), keyEquivalent: "P")
+    publishMenuItem = menu.addItemWithTitle("Publish My Clipboard", action: Selector("publishClipboard:"), keyEquivalent: "P")!
     menu.addItem(NSMenuItem.separatorItem())
     disconnectMenuItem = menu.addItemWithTitle("Disconnect", action: Selector("disconnect:"), keyEquivalent: "D")!
+    connectMenuItem = menu.addItemWithTitle("Connect", action: Selector("connect:"), keyEquivalent: "C")!
     menu.addItemWithTitle("Quit", action: Selector("terminate:"), keyEquivalent: "q")
     return menu
-  }
-    
-  private func publishClipboard(sender: AnyObject) {
-    client.publishLocalClipboard()
   }
   
   private func showNotification(title: String, body: String) {
